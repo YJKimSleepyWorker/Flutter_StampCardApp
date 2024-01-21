b9991614d627e9daa64d4333efb3a680d7212942
yongjun.kim <dydwns5236@gmail.com>
2024-01-21 13:31:47 +0900

    DevicePreview를 도입, 첫 실행

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      tools: [...DevicePreview.defaultTools],
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
